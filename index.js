f7c70973a556178f90edbc275edffd324d445ed1
Call333 <cam4@aluno.ifal.edu.br>
2026-08-20 09:22:37 -0300

    feat(rota-01): responde GET / com 'Olá, Mundo!'

--- index.js
+++ index.js
@@ -19,3 +19,28 @@
 // });
 //
 // server.listen(PORT, () => console.log(`Servidor em http://localhost:${PORT}`));
+
+import http from 'node:http';
+
+const host = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer(
+
+    (req, res) => {
+        if (req.method === "GET") {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end("Olá, Mundo!");
+            return;
+        }
+
+        res.writeHead(404);
+        res.end();
+    }
+)
+
+server.listen(
+    port, host, () => {
+        console.log(`Servidor rodando em http://${host}:${port}/`)
+    }
+)
